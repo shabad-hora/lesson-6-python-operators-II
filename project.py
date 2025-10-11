541c29e1ba8f703093f8df657a5c7c02f9bd3fab
shabad-hora <aashihora20@gmail.com>
2025-10-11 16:31:43 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,5 @@
+char=input("enter the character:")
+if ((char[0]>"a" and char[0]<"z") or (char[0]>"A" and char[0]>"Z")):
+    print("the given character is an alphabet")
+else:
+    print("the given character is not an alphabet")
